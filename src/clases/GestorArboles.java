4c88d7c90f5a5d1218949ee06ddb516acf8aa29c
leireesee <leisancabalei@gmail.com>
2023-01-23 21:35:20 +0100

    v2 - terminada

--- src/clases/GestorArboles.java
+++ src/clases/GestorArboles.java
@@ -6,6 +6,7 @@ import java.sql.Statement;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -72,8 +73,17 @@ public class GestorArboles {
 					arbol.setOrigen(origenIntro);
 					/*INSERT INTO `arboles` (`id`, `nombre_comun`, `nombre_cientifico`, `habitat`, `altura`, `origen`) VALUES (NULL, '767676', 'turfu', 'ryr5', '5', 'gdsrhd');*/
 					
-					String sentenciaInsert = "INSERT INTO arboles (nombre_comun, nombre_cientifico, habitat, altura, origen) VALUES ('"+ nombreComunIntro +"',' "+nombreCientificoIntro +"','"+ habitatIntro+"'," + alturaIntro + ",'" + origenIntro + "');";
-				st.execute(sentenciaInsert);	
+					PreparedStatement pst = conexion.prepareStatement("INSERT INTO arboles (nombre_comun, nombre_cientifico, habitat, altura, origen) VALUES (?, ?, ?, ?, ?);");
+					
+					pst.setString(1, arbol.getNombreComun());
+					pst.setString(2, arbol.getNombreCientifico());
+					pst.setString(3, arbol.getHabitat());
+					pst.setInt(4, arbol.getAltura());
+					pst.setString(5, arbol.getOrigen());
+					pst.execute();
+					
+					/*String sentenciaInsert = "INSERT INTO arboles (nombre_comun, nombre_cientifico, habitat, altura, origen) VALUES ('"+ nombreComunIntro +"',' "+nombreCientificoIntro +"','"+ habitatIntro+"'," + alturaIntro + ",'" + origenIntro + "');";
+				st.execute(sentenciaInsert);*/
 					break;
 				case ELIMINAR_ARBOL:
 					System.out.println("Has elegido eliminar arbol");
@@ -84,6 +94,8 @@ public class GestorArboles {
 					break;
 				case MODIFICAR_ARBOL:
 					System.out.println("Has elegido modificar arbol");
+					
+					Arbol arbol2 = new Arbol();
 					
 					System.out.println("Introduce la ID del arbol a modificar");
 					String id_modificar = teclado.nextLine();
@@ -103,10 +115,27 @@ public class GestorArboles {
 					System.out.println("Introduce el nuevo origen");
 					String origenIntro2 = teclado.nextLine();
 					
+					arbol2.setNombreComun(nombreComunIntro2);
+					arbol2.setNombreCientifico(nombreCientificoIntro2);
+					arbol2.setHabitat(habitatIntro2);
+					arbol2.setAltura(alturaIntro2);
+					arbol2.setOrigen(origenIntro2);
 					
-					String sentenciaModificar = "UPDATE arboles SET nombre_comun = '" + nombreComunIntro2 + "', nombre_cientifico = '" + nombreCientificoIntro2 + "', habitat = '" + habitatIntro2 + "', altura = " + alturaIntro2 + ", origen = '" + origenIntro2 + "' WHERE id = " + id_modificar;
 					
-					st.executeUpdate(sentenciaModificar);
+					PreparedStatement pst2 = conexion.prepareStatement("UPDATE arboles SET nombre_comun = ?, nombre_cientifico = ?, habitat = ?, altura = ?, origen = ? WHERE id = ?");
+					
+					pst2.setString(1, arbol2.getNombreComun());
+					pst2.setString(2, arbol2.getNombreCientifico());
+					pst2.setString(3, arbol2.getHabitat());
+					pst2.setInt(4, arbol2.getAltura());
+					pst2.setString(5, arbol2.getOrigen());
+					pst2.setString(6, id_modificar);
+					pst2.executeUpdate();
+					
+					
+					/*String sentenciaModificar = "UPDATE arboles SET nombre_comun = '" + nombreComunIntro2 + "', nombre_cientifico = '" + nombreCientificoIntro2 + "', habitat = '" + habitatIntro2 + "', altura = " + alturaIntro2 + ", origen = '" + origenIntro2 + "' WHERE id = " + id_modificar;
+					
+					st.executeUpdate(sentenciaModificar);*/
 					break;
 				case VISUALIZAR_ARBOLES:
 					System.out.println("Has elegido visualizar arboles");
